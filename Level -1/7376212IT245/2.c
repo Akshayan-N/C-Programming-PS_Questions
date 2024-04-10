fe7343001b86d26b0731bce6e6d388aa0d7b0626
☯ Quantum Shadow ☯ <126897643+Quantum-Shadow@users.noreply.github.com>
2024-04-11 03:14:14 +0530

    Add files via upload

---
+++ Level -1/7376212IT245/2.c
@@ -0,0 +1,27 @@
+// 2.greatest of three numbers
+
+#include <stdio.h>
+
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+int main(void)
+{
+    int number[3];
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Enter Number %i : ", i+1);
+        scanf("%i", &number[i]);
+    }
+
+    int max_number = number[0];
+    for (int i = 1; i < 3; i++)
+    {
+        max_number = max(max_number, number[i]);
+    }
+    
+    printf("Max Value : %i", max_number);
+
+    return 0;
+}
