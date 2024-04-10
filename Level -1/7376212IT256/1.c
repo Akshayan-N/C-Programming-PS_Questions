8de1a4260377351bf60ed7ccaa4cc55be0a10a4d
☯ Quantum Shadow ☯ <126897643+Quantum-Shadow@users.noreply.github.com>
2024-04-11 03:20:31 +0530

    Add files via upload

---
+++ Level -1/7376212IT256/1.c
@@ -0,0 +1,20 @@
+// 1. To find the given input is alphabet or not 
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char letter;
+    printf("Enter the letter : ");
+    scanf("%c", &letter);
+
+    if (isalpha(letter))
+    {
+        printf("Alphabet");
+    }
+    else
+    {
+        printf("Not A Alphabet");
+    }
+}
