986a9f596c0725591dd3a3fa42ea00656c7981d6
☯ Quantum Shadow ☯ <126897643+Quantum-Shadow@users.noreply.github.com>
2024-04-10 23:55:46 +0530

    Add files via upload

---
+++ 7376212IT167/1.c
@@ -0,0 +1,29 @@
+// 1.To find compound interest .the formula is given
+
+#include <stdio.h>
+#include <math.h>
+
+void main(void)
+{
+    
+    float principal, total_amount, interest_rate;
+    int nos_times, time_period;
+
+    printf("Enter the principal amount : ");
+    scanf("%f", &principal);
+
+    printf("Enter the interest rate : ");
+    scanf("%f", &interest_rate);
+
+    printf("Enter the number of times to apply interest : ");
+    scanf("%i", &nos_times);
+
+    printf("Enter the time period : ");
+    scanf("%i", &time_period);
+
+
+    total_amount = principal * pow(1 + interest_rate / ((float) nos_times * 100), nos_times * time_period);
+
+    printf("Total compound amount is %.2f", total_amount);
+
+}
