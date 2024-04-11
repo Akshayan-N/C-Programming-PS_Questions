fd4670d795df610837a4390b57755aafdef380c7
☯ Quantum Shadow ☯ <126897643+Quantum-Shadow@users.noreply.github.com>
2024-04-11 13:11:27 +0530

    Add files via upload

---
+++ Level -1/7376212IT131/1.c
@@ -0,0 +1,28 @@
+// 1. Find the shipping cost of the product by multiplying the given cost value according to the range.
+
+#include <stdio.h>
+
+int main(void)
+{
+    float cost, shipping_cost;
+    
+    printf("Enter the cost : ");
+    scanf("%f", &cost);
+    
+    if (cost <= 100)
+    {
+        shipping_cost = cost * 0.05;
+    }
+    else if (cost > 100 && cost <= 500)
+    {
+        shipping_cost = cost * 0.07;
+    }
+    else
+    {
+        shipping_cost = cost * 0.10;
+    }
+    
+    printf("Shipping cost: $%.2f\n", shipping_cost);
+
+    return 0;
+}
---
+++ Level -1/7376212IT131/2.c
@@ -0,0 +1,45 @@
+    //  2. To print whether the given two inputs are even, odd or neither even or odd. 
+
+    #include <stdio.h>
+    #include <ctype.h>
+    #include <stdlib.h>
+
+    #define MAX_LENGTH 20
+
+    int main(void)
+    {
+        char number[MAX_LENGTH];
+        printf("Enter the number : ");
+        scanf("%s", number);
+        
+        int flag = 1;
+        int last_digit;
+        for (int i = 0; number[i] != '\0'; i++)
+        {
+            if (!isdigit(number[i]))
+            {
+                flag = 0;
+                break;
+            }
+            if (number[i+1] == '\0')
+            {
+                last_digit = atoi(&number[i]);
+            }
+        }
+        if (flag)
+        {
+            if (last_digit % 2)
+            {
+                printf("ODD");
+            }
+            else
+            {
+                printf("EVEN");
+            }
+        }
+        else
+        {
+            printf("Neither Even or Odd");
+            return 0;
+        }
+    }
